da5e690abf292002c8b2d956f585692199a7c4dc
JOAO PEDRO FERREIRA MACIEL <n226814168@facli0419.LAN>
2019-09-12 22:03:46 -0300

    criando lógica para a listagem de chats

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,9 +8,34 @@ import { chatsSeed } from '../../services/seeds';
 })
 export class Tab1Page {
 
-  public chats = chatsSeed;
+  public chats = [];
+  public recentChats = [];
+  public onlineChats = [];
+  public offlineChats = [];
+
   constructor() {
-    console.log(this.chats);
+    this.chats = chatsSeed;
+    this.loadRecentChats();
+    this.loadOnlineChats();
+    this.loadOfflineChats();
   }
 
+  private loadRecentChats(){
+    this.recentChats = this.chats;
+    
+    this.recentChats.sort(function(a, b) {
+      const dateA:any = new Date(a.dataUltimaMsg);
+      const dateB:any = new Date(b.dataUltimaMsg);
+      return dateB - dateA;
+  });
+  this.recentChats = this.recentChats.slice(0,4);
+  }
+  
+  private loadOnlineChats(){
+    this.onlineChats = this.chats.filter(x=>x.status==='online');    
+  }
+  
+  private loadOfflineChats(){
+    this.offlineChats = this.chats.filter(x=>x.status==='offline');    
+  }
 }
