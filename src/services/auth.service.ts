bd9e9fecc4380c0a5dee0989ee778884fb94dd36
João Mago <joao.mago@drlavatudo.com>
2019-11-18 22:45:05 -0300

    feat: adding firebase2 into package.json

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,20 +1,28 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { of as ObservableOf, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { AngularFireAuth } from "angularfire2/auth";
+import { auth } from "firebase";
 
-import { auth } from 'firebase/app';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-
-@Injectable({ providedIn: 'root' })
+@Injectable()
 export class AuthService {
+  public user: Observable<firebase.User>;
 
-    constructor(
-        private afAuth: AngularFireAuth,
-        private afs: AngularFirestore,
-        private router: Router
-    ) { }
+  constructor(private router: Router, private afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
 
+  signup(login: string, senha: string) {
+    this.afAuth.auth.createUserWithEmailAndPassword(login, senha);
+  }
+
+  login(login: string, senha: string) {
+    this.afAuth.auth.signInWithEmailAndPassword(login, senha);
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+    this.router.navigate(["/login"]);
+  }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,37 +1,41 @@
-import { AngularFireDatabase } from '@angular/fire/database';
-import { map } from 'rxjs/operators';
-import { Injectable } from '@angular/core';
-import {User} from '../model'
+import { map } from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { User } from "../model";
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
 
 export class UserService {
-    constructor(private db: AngularFireDatabase){
-    }
+  constructor(private db: AngularFireDatabase) {}
 
-    insert(User: User) {
-        this.db.list('User').push(User)
-          .then((result: any) => {
-            console.log(result.key);
-          });
-      }
-    
-      update(User: User, key: string) {
-        this.db.list('User').update(key, User)
-          .catch((error: any) => {
-            console.error(error);
-          });
-      }
-    
-      getAll() {
-        return this.db.list('User')
-          .snapshotChanges()
-          .pipe(
-            map(changes => {
-              return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
-            })
-          );
-      }
-    
-      delete(key: string) {
-        this.db.object(`User/${key}`).remove();
-      }
+  insert(User: User) {
+    this.db
+      .list("User")
+      .push(User)
+      .then((result: any) => {
+        console.log(result.key);
+      });
+  }
+
+  update(User: User, key: string) {
+    this.db
+      .list("User")
+      .update(key, User)
+      .catch((error: any) => {
+        console.error(error);
+      });
+  }
+
+  getAll() {
+    return this.db
+      .list("User")
+      .snapshotChanges()
+      .pipe(
+        map(changes => {
+          return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+        })
+      );
+  }
+
+  delete(key: string) {
+    this.db.object(`User/${key}`).remove();
+  }
 }
